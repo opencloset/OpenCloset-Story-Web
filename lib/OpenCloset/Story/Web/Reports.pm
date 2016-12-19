9d42f1b395f89768217ee15319d02d746c39dcbd
Keedi Kim <keedi.k@gmail.com>
2016-12-19 19:12:32 +0900

    Support search query

--- lib/OpenCloset/Story/Web/Reports.pm
+++ lib/OpenCloset/Story/Web/Reports.pm
@@ -15,6 +15,50 @@ sub donation_get {
     );
 }
 
+sub donation_post {
+    my $self = shift;
+
+    my $q    = $self->param("q");
+    my $name = $q;
+    ( my $phone = $q ) =~ s/\D//g;
+
+    my @cond;
+    if ( length $name >= 2 ) {
+        push @cond, +{ "user.name" => $name };
+    }
+    if ( length $phone >= 7 ) {
+        push @cond, +{ "user_info.phone" => $phone };
+    }
+
+    unless (@cond) {
+        $self->render(
+            template     => "reports-d",
+            donation_rs  => undef,
+            preview_size => 128,
+            q            => $q,
+        );
+        return;
+    }
+
+    my $limit = 24;
+
+    my $donation_rs = $self->schema->resultset("Donation")->search(
+        { -or => \@cond },
+        {
+            join     => { "user" => "user_info" },
+            order_by => { -desc  => "me.id" },
+            rows     => $limit,
+        },
+    );
+
+    $self->render(
+        template     => "reports-d",
+        donation_rs  => $donation_rs,
+        preview_size => 128,
+        q            => $q,
+    );
+}
+
 1;
 
 __END__
